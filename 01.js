6063c2a469e63fb83293e9c25c7ba1a97e5ce20f
rmakulik <kulikr.js@mail.ru>
2024-05-17 10:17:37 +0300

    01

---
+++ 01.js
@@ -0,0 +1,18 @@
+function getGrade (s1, s2, s3) {
+    
+    let res = (s1 + s2 + s3) / 3;
+
+    if(res >= 90 && res <= 100 ) {
+        return 'A';
+    } else if(res >= 80 && res <= 90 ) {
+        return 'B';
+    } else if(res >= 70 && res <= 80 ) {
+        return 'C';
+    } else if(res >= 60 && res <= 70 ) {
+        return 'D';
+    } else if(res >= 0 && res <= 60 ) {
+        return 'F';
+    }
+  }
+
+  console.log(getGrade(70,70,100));
