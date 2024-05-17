f5f928ea46521b13786474991498a67797a20830
rmakulik <kulikr.js@mail.ru>
2024-05-17 12:26:54 +0300

    02

--- 01.js
+++ 01.js
@@ -2,15 +2,15 @@ function getGrade (s1, s2, s3) {
     
     let res = (s1 + s2 + s3) / 3;
 
-    if(res >= 90 && res <= 100 ) {
+    if(res >= 90) {
         return 'A';
-    } else if(res >= 80 && res <= 90 ) {
+    } else if(res >= 80) {
         return 'B';
-    } else if(res >= 70 && res <= 80 ) {
+    } else if(res >= 70) {
         return 'C';
-    } else if(res >= 60 && res <= 70 ) {
+    } else if(res >= 60) {
         return 'D';
-    } else if(res >= 0 && res <= 60 ) {
+    } else if(res >= 0) {
         return 'F';
     }
   }
