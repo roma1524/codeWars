96a3ee7959e8219ba7f7ac03b1dd2b133b29bca8
romario <kulikr.js@mail.ru>
2024-12-18 09:55:36 +0300

    7kyu 554

--- 7kyu/7kyu.js
+++ 7kyu/7kyu.js
@@ -28,26 +28,25 @@ In mathematics, a square number or perfect square is an integer that is the squa
 
 
 // var isSquare = function(n){
-    /*if(n < 0) {
-        return false;
-    }
-    if(n === 0) {
+/*if(n < 0) {
+    return false;
+}
+if(n === 0) {
+    return true;
+}
+if(n === 1) {
+    return true;
+}
+for(let i = 1; i < n; i++) {
+    if (n % i === 0 && i * i === n) {
         return true;
     }
-    if(n === 1) {
-        return true;
-    }
-    for(let i = 1; i < n; i++) {
-        if (n % i === 0 && i * i === n) {
-            return true;
-        }
-    }
-    return false*/
-    // return Math.sqrt(n) % 1 === 0
+}
+return false*/
+// return Math.sqrt(n) % 1 === 0
 // }
 
 // console.log(isSquare(26))
-
 
 
 // This time no story, no theory. The examples below show you how to write function accum:
@@ -70,9 +69,7 @@ return result.join('-')*/
 // console.log(accum("ZpglnRxqenU"))
 
 
-
 // ============================================================================================================
-
 
 
 /*Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
@@ -106,36 +103,32 @@ console.log(maskify('4556364607935616').length);*/
 //     The input array will always be valid! (odd-length >= 3)
 
 // function stray(numbers) {
-   /* const res = {};
-    for (let i = 0; i < numbers.length; i++) {
-        if (!res[numbers[i]]) {
-            res[numbers[i]] = 1
-        } else {
-            res[numbers[i]] += 1
-        }
-    }
-    for(let key in res) {
-        if(res[key] === 1) {
-            return +key;
-        }
-    }*/
+/* const res = {};
+ for (let i = 0; i < numbers.length; i++) {
+     if (!res[numbers[i]]) {
+         res[numbers[i]] = 1
+     } else {
+         res[numbers[i]] += 1
+     }
+ }
+ for(let key in res) {
+     if(res[key] === 1) {
+         return +key;
+     }
+ }*/
 
 
+/*for (var i in numbers){
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+}*/
 
 
-    /*for (var i in numbers){
-        if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
-    }*/
+/*var a = numbers.sort();
 
-
-
-
-    /*var a = numbers.sort();
-
-    if(a[0] != a[1]) {
-        return a[0]
-    }
-    return a[a.length-1]*/
+if(a[0] != a[1]) {
+    return a[0]
+}
+return a[a.length-1]*/
 }
 
 // console.log(stray([1, 2, 1]))
@@ -171,7 +164,6 @@ console.log(arithmetic(5, 2, "multiply"))
 console.log(arithmetic(8, 2, "divide"))*/
 
 
-
 // As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 //
 //     The input to the function will be an array of three distinct numbers (Haskell: a tuple).
@@ -203,7 +195,6 @@ console.log(gimme([5.9, 10.4, 14.2]))*/
 // };
 //
 // console.log(capitals('CodEWaRs'))
-
 
 
 // The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
@@ -291,8 +282,6 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
 console.log(reverseLetter("r9gpzuk$lwm1flvaetilez_@czcz8!.njp-yynulc"))*/
 
 
-
-
 // The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
 //
 // The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
@@ -330,7 +319,6 @@ console.log(reverseLetter("r9gpzuk$lwm1flvaetilez_@czcz8!.njp-yynulc"))*/
 //
 //
 // console.log(sumDigits(-32))
-
 
 
 // In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
@@ -395,7 +383,6 @@ Task
 // console.log(minValue([1, 3, 1]))
 
 
-
 // Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
 //
 //     For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
@@ -440,7 +427,6 @@ Task
 // console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
 
 
-
 // Given a sequence of numbers, find the largest pair sum in the sequence.
 
 
@@ -462,7 +448,6 @@ Task
 // }
 //
 // console.log(noOdds([-59,169,186,11,200,104,-39,155,9,91,140,-99,177,31,1,119,-194,-155,-140,137,-195,-27,-100,182,-199,-144,-22,76,-88,-47,101,139,176,-19,-31,57,-74,58,-168,125,-102,191,169,200,-80,-95,35,51,-82,196,-157,44,-165,-105,3,-65,12,-12,114,17,38,128,-145,188,-61,90,173,-60,198,60,-150,84,-14,4,169,100,-6,-99,-108,-189,-34,-134,150,-18,144,107,-191,-94,-150,151,82,174,171,-177,174,-19,-51,142,98,-94,-3,189,164,-137,-132,-138,-27,-18,-1,-162,199,-184,25,135,-67,187,-34,-111,-27,-98,-39,181,-35,25,-95,-140,6,-187,-199,76,166,-193,-128,-68,166,-151,-187,-64,-136,74,4,-151,26,-95,166,-164,-75,2,-22,-57,179,-154,-112,6,193,-100,-18,23,-196,45,52,-67,-1,9,168,-139,-177,138,133,-199,96,-12,-148,-5,177,-156,58,42,-161,-61,-93,-122,-28,-56,-161]))
-
 
 
 // Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
@@ -527,7 +512,6 @@ Task
 // console.log(isSortedAndHow([4, 2, 30]))
 
 
-
 // My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
 //
 //     In honor of my grandfather's memory we will write a function using his formula!
@@ -564,8 +548,179 @@ Task
 // Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
 //     Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
-function bump(x){
-    return x.split('_').length
-}
+// function bump(x){
+//     return x.split('_').length
+// }
+//
+// console.log(bump("__nn_nnnn__n_n___n____nn__nnn"))
 
-console.log(bump("__nn_nnnn__n_n___n____nn__nnn"))
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+//
+//     All inputs will be valid.
+
+
+// function digits(n) {
+//     return n.toString().split('').length
+// }
+//
+// console.log(digits(9007199254740991))
+
+
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+//
+//     Replace certain values however if any of the following conditions are met:
+//
+//     If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+
+// function fizzbuzz(n) {
+//     const arr = [];
+//
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 5 === 0 & i % 3 === 0) {
+//             arr.push('FizzBuzz')
+//         } else if (i % 5 === 0) {
+//             arr.push('Buzz')
+//         } else if (i % 3 === 0) {
+//             arr.push('Fizz')
+//         } else {
+//             arr.push(i)
+//         }
+//     }
+//     return arr;
+// }
+//
+//
+// console.log(fizzbuzz(77))
+
+
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+//
+//     Example:
+//
+// Input:
+//
+//     'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+//
+// Output:
+//
+//     'alpha beta gamma delta'
+
+// function removeDuplicateWords (s) {
+//     return Array.from(new Set(s.split(' '))).join(' ')
+// }
+//
+// console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
+
+
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+//
+// The solution would work like the following:
+
+// function getEvenNumbers(numbersArray){
+//     return numbersArray.filter(el => el % 2 === 0)
+// }
+//
+// console.log(getEvenNumbers([1,2,3,6,8,10]))
+
+
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+
+// var greet = function(name) {
+//     let gf = name.toLowerCase().split('').map((el, i) => {
+//         if(i === 0) {
+//             return el.toUpperCase();
+//         }
+//         else {
+//             return el
+//         }
+//     }).join('')
+//     return `Hello ${gf}!`;
+// };
+//
+// console.log(greet('BILLY'))
+
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+//
+//     For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+//
+//     [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+// function sumTwoSmallestNumbers(numbers) {
+//     const res = numbers.sort((a, b) => a - b)
+//     return res[0] + res[1];
+// }
+//
+// console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
+
+
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+//
+//     If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// function friend(friends){
+//     return friends.filter(el => el.length === 4);
+// }
+//
+// console.log(friend(["Ryan", "Kieran", "Mark"]))
+
+
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+
+// function divisors(integer) {
+//     const res = [];
+//     for(let i = 2; i < integer; i++) {
+//         if(integer % i === 0) {
+//             res.push(i)
+//         }
+//     }
+//     return res.length ? res : `${integer} is prime`;
+// }
+//
+// console.log(divisors(15))
+
+
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+// function dontGiveMeFive(start, end) {
+//     const res = [];
+//     for(let i = start; i <= end; i++) {
+//         if(!i.toString().includes('5')) res.push(i);
+//     }
+//
+//     return res.length
+// }
+//
+// console.log(dontGiveMeFive(4,100))
+
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+
+// function solution(nums){
+//     return nums?.length || nums !== null ? nums.sort((a, b) => a - b) : [];
+// }
+//
+// console.log(solution([1,2,3,10,5]))
+
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+//
+//     You can assume all values in the array are numbers.
+
+// function smallEnough(a, limit){
+//     const res = [...a].filter(el => el <= limit);
+//     return a.length === res.length
+// }
+//
+// console.log(smallEnough([66, 101], 200))
